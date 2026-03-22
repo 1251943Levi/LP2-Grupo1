69f92c3c9fb2c62713f0e6c09174001e221dfce3
1251666SaraLondreira <1251666@isep.ipp.pt>
2026-03-22 17:17:15 +0000

    Implementar classe Estudante com heranca de Utilizador e atributos especificos

---
+++ LP2-Grupo1/src/model/Estudante.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Estudante extends Utilizador {
+
+    // ---------- ATRIBUTOS ESPECÍFICOS ----------
+    private final int numeroMecanografico;
+    private final int anoPrimeiraInscricao;
+
+    // ---------- CONSTRUTOR ----------
+    public Estudante(){}
+    public Estudante(int numeroMecanografico, String email, String password, String nome, String nif, String morada, String dataNascimento, int anoPrimeiraInscricao) {
+        super(email, password, nome, nif, morada, dataNascimento);
+        this.numeroMecanografico = numeroMecanografico;
+        this.anoPrimeiraInscricao = anoPrimeiraInscricao;
+    }
+
+    // ---------- GETTERS ----------
+    public int getNumeroMecanografico() { return numeroMecanografico; }
+    public int getAnoPrimeiraInscricao() { return anoPrimeiraInscricao; }
+}
