f23534826dcc909212a8bf33854d7d5808c7dc87
1251943Levi <1251943@isep.ipp.pt>
2026-03-22 22:48:30 +0000

    Instanciar MainView e repositorio de dados

--- LP2-Grupo1/src/Controller/MainController.java
+++ LP2-Grupo1/src/Controller/MainController.java
@@ -1,4 +1,17 @@
 package Controller;
 
+import View.MainView;
+
 public class MainController {
+
+    // ---------- ATRIBUTOS ----------
+    private MainView view;
+    private RepositorioDados repositorio;
+
+    // ---------- CONSTRUTOR ----------
+    public MainController() {
+        this.view = new MainView();
+        this.repositorio = new RepositorioDados();
+
+
 }
