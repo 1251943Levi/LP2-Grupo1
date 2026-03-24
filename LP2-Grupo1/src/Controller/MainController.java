fce0e914c7450fa036ed6dcdc9e8cf5b6af1f075
1251943Levi <1251943@isep.ipp.pt>
2026-03-24 22:54:42 +0000

    Atualizaçao do codigo , adicionado package Controller e View e as class

--- LP2-Grupo1/src/Controller/MainController.java
+++ LP2-Grupo1/src/Controller/MainController.java
@@ -1,17 +1,41 @@
 package Controller;
 
 import View.MainView;
+import Model.RepositorioDados;
 
 public class MainController {
 
-    // ---------- ATRIBUTOS ----------
     private MainView view;
     private RepositorioDados repositorio;
 
-    // ---------- CONSTRUTOR ----------
     public MainController() {
         this.view = new MainView();
         this.repositorio = new RepositorioDados();
+    }
 
+    public void iniciarSistema() {
+        boolean aExecutar = true;
+        view.mostrarMensagem("Bem-vindo ao Sistema do ISSMF!");
 
+        while (aExecutar) {
+            int opcao = view.mostrarMenu();
+
+            switch (opcao) {
+                case 1:
+                    view.mostrarMensagem("\n--- LOGIN DO SISTEMA ---");
+                    // O código avançado de login entra em commits futuros
+                    break;
+                case 2:
+                    view.mostrarMensagem("\n--- NOVO REGISTO DE ESTUDANTE ---");
+                    // A chamada para criarEstudanteSemLogin() entra noutro commit
+                    break;
+                case 0:
+                    view.mostrarMensagem("A encerrar o sistema...");
+                    aExecutar = false;
+                    break;
+                default:
+                    view.mostrarMensagem("Opção inválida.");
+            }
+        }
+    }
 }
--- LP2-Grupo1/src/View/MainView.java
+++ LP2-Grupo1/src/View/MainView.java
@@ -3,28 +3,18 @@ package View;
 import java.util.Scanner;
 
 public class MainView {
-
-    // ---------- ATRIBUTOS ----------
     private Scanner scanner;
 
-    // ---------- CONSTRUTOR ----------
     public MainView() {
         this.scanner = new Scanner(System.in);
     }
 
-    // ---------- MÉTODOS DE APRESENTAÇÃO E LEITURA ----------
-
-    /**
-     * Apresenta o menu de entrada principal da aplicação.
-     * @return A opção numérica escolhida pelo utilizador. Retorna -1 em caso de input inválido.
-     */
     public int mostrarMenu() {
         System.out.println();
         System.out.println("===== SISTEMA ISSMF =====");
         System.out.println("1 - Login");
         System.out.println("2 - Criar Estudante");
-        System.out.println("3 - Sair");
-
+        System.out.println("0 - Sair");
         System.out.print("Opção: ");
 
         try {
@@ -34,21 +24,13 @@ public class MainView {
         }
     }
 
-    /**
-     * Imprime uma mensagem no ecrã e aguarda que o utilizador introduza uma resposta em texto.
-     * @param mensagem Texto a ser exibido antes do pedido de input.
-     * @return A string inserida pelo utilizador.
-     */
     public String pedirInputString(String mensagem) {
         System.out.print(mensagem + ": ");
         return scanner.nextLine();
     }
 
-    /**
-     * Apresenta uma mensagem padronizada de feedback no ecrã.
-     * @param mensagem O conteúdo a exibir.
-     */
     public void mostrarMensagem(String mensagem) {
         System.out.println(">> " + mensagem);
     }
 }
+
