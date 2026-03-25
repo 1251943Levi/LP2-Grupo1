592f6855ead7d7fea7e794fe2dbf22c784461f8c
1251943Levi <1251943@isep.ipp.pt>
2026-03-25 01:08:19 +0000

    comentarios apagados

--- LP2-Grupo1/src/Main.java
+++ LP2-Grupo1/src/Main.java
@@ -3,8 +3,6 @@ import Controller.MainController;
 
 public class Main {
     public static void main(String[] args) {
-        // --- Diretório para colocar o dados.csv ---
-        System.out.println(">>> O Java está a procurar ficheiros na pasta: " + System.getProperty("user.dir"));
 
         MainController mc = new MainController();
         mc.iniciarSistema();
