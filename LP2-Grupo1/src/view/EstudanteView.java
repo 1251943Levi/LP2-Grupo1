7e313cf3ac8838b3b29089dd388330f9ec9d07ab
1251943Levi <1251943@isep.ipp.pt>
2026-03-31 00:23:04 +0100

    Menu Criado

--- LP2-Grupo1/src/view/EstudanteView.java
+++ LP2-Grupo1/src/view/EstudanteView.java
@@ -1,4 +1,28 @@
 package view;
 
+import java.util.Scanner;
+
 public class EstudanteView {
+
+    private Scanner scanner;
+
+    public EstudanteView() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public int mostrarMenuPrincipal() {
+        System.out.println("\n=== MENU ESTUDANTE ===");
+        System.out.println("1 - Ver Dados Pessoais");
+        System.out.println("2 - Atualizar Dados");
+        System.out.println("3 - Percurso Académico");
+        System.out.println("0 - Sair / Logout");
+        System.out.print("Escolha uma opção: ");
+
+        try {
+            return Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            return -1; // Devolve -1 se o utilizador escrever letras em vez de números
+        }
+    }
+
 }
