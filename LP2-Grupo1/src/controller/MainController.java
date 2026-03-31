5df4c38be1e6a97250c523f4d1668cf9076b6ac0
1251943Levi <1251943@isep.ipp.pt>
2026-03-31 19:56:18 +0100

    removoçao de comentarios

--- LP2-Grupo1/src/controller/MainController.java
+++ LP2-Grupo1/src/controller/MainController.java
@@ -24,16 +24,15 @@ public class MainController {
             switch (opcao) {
                 case 1:
                     String email = view.pedirInputString("Email");
+                    String pass = view.pedirInputString("Password");
 
-                    // 2. Hack Provisório para a Pessoa 2 (Gestor) conseguir trabalhar!
-                    // Como ainda não há ficheiros CSV, deixamos o admin entrar por uma "porta das traseiras"
+
                     if (email.equals("admin@issmf.pt") && pass.equals("admin123")) {
                         view.mostrarMensagem("Login de Gestor detetado! A reencaminhar...");
-                        // new controller.GestorController(repositorio, new model.Gestor("admin@issmf.pt", "admin123", "Admin", "000000000", "Sede", "01/01/1980")).iniciar();
                         break;
                     }
 
-                    // 3. Autenticar Estudantes e Docentes normais
+
                     model.Utilizador userLogado = repositorio.autenticar(email, pass);
 
                     if (userLogado == null) {
@@ -41,15 +40,16 @@ public class MainController {
 
                     } else if (userLogado instanceof model.Estudante) {
                         view.mostrarMensagem("Login de Estudante detetado! A reencaminhar...");
-                        // new controller.EstudanteController(repositorio, (model.Estudante) userLogado).iniciar();
+
 
                     } else if (userLogado instanceof model.Docente) {
                         view.mostrarMensagem("Login de Docente detetado! A reencaminhar...");
-                        // new controller.DocenteController(repositorio, (model.Docente) userLogado).iniciar();
+
                     }
                     break;
                 case 2:
                     view.mostrarMensagem("\n--- NOVO REGISTO DE ESTUDANTE ---");
+
                     break;
                 case 0:
                     view.mostrarMensagem("A encerrar o sistema...");
